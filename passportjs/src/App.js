57c3ab7d7c4f1c8767dff24220ad37fb567ee7a3
muddassirk <mkkhanu72@gmail.com>
2021-04-06 15:17:59 +0500

    login page

---
+++ passportjs/src/App.js
@@ -0,0 +1,15 @@
+
+import Login from './components/Login'
+import Signup from './components/signup'
+
+
+function App() {
+  return (
+    <>
+    <Login />
+    {/* <Signup /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ passportjs/src/components/signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@material-ui/core';
+
+function Signup() {
+    return (
+        <div>
+          <Button color="primary">Hello Signup</Button>;      
+            
+        </div>
+    )
+}
+
+export default Signup;
